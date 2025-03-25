e81f245c659a8630a05d948cf120b4c5c1ddc6e4
Aaronrashid <anr.ron193@gmail.com>
2025-03-25 22:59:03 +0300

    updates index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
+  let form = document.querySelector('#create-task-form');
+  let formInput = document.querySelector('#new-task-description');
+  let taskList = document.querySelector('#tasks');
+
+  const taskText = formInput.value.trim();
+
+  formInput.addEventListener("click", (event) =>
+  event.preventDefault())
+
+  const newLi = document.createElement("li");
+  newLi.className = "task";
+
+  const taskInput = document.createElement("input");
+  taskInput.type = "text";
+  taskInput.disabled = true;
+
+  taskInput.value = taskText;
+
+  newLi.appendChild(taskInput);
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.innerText = "Delete";
+  deleteBtn.className = "deleteBtn";
+
+  newLi.appendChild(deleteBtn)
+  taskList.appendChild(newLi);
   // your code here
 });
